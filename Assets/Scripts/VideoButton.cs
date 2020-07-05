785ba87033f694cf85a72b132aa192be1329134d
Anca Tutescu <anca-stefania.tutescu@haw-hamburg.de>
2020-07-05 02:51:05 +0200

    Added Planetarium videos; added planetarium video selection thumbnails & captions; added events to play video and/or sounds in planetarium, lobby, mapping & light room

--- Assets/Scripts/VideoButton.cs
+++ Assets/Scripts/VideoButton.cs
@@ -9,30 +9,40 @@ public class VideoButton : UIButton
 {
     public string videoName;
     public Canvas canvas;
+    Animator canvasAnim;
     public MediaPlayer mediaPlayer;
+    public PlanetariumSoundManager pManager;
+    public int buttonID;
 
     private void Start()
     {
+        canvasAnim = canvas.GetComponent<Animator>();
         mediaPlayer.Events.AddListener(OnMediaPlayerEvent);
     }
 
     public override void OnClick(Vector3 hitPoint)
     {
         base.OnClick(hitPoint);
+        PlayVideo(buttonID);
+    }
+
+    public void PlayVideo(int videoID)
+    {
         mediaPlayer.OpenVideoFromFile(MediaPlayer.FileLocation.RelativeToStreamingAssetsFolder, Application.streamingAssetsPath + videoName, true);
         mediaPlayer.m_Loop = false;
-        canvas.enabled = false;
         mediaPlayer.Play();
+        pManager.PlaySound(videoID);
     }
 
     public void OnMediaPlayerEvent(MediaPlayer mp, MediaPlayerEvent.EventType et, ErrorCode errorCode)
     {
         switch (et)
         {
-            case MediaPlayerEvent.EventType.Started:                
+            case MediaPlayerEvent.EventType.Started:
+                canvasAnim.SetTrigger("fadeOut");
                 break;
             case MediaPlayerEvent.EventType.FinishedPlaying:
-                canvas.enabled = true;
+                canvasAnim.SetTrigger("fadeIn");
                 break;
         }
 
